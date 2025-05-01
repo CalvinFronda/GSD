7df4334280c8273bf36c50bd42cfc6b8a11700fc
Calvin Fronda <cfronda209@gmail.com>
2025-05-01 12:50:51 -0700

    feat: inital vite install, tailwind, shadcn & react-ts

---
+++ README.md
@@ -0,0 +1,1 @@
+# Getting stuff down
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/shared/ui/Button/Button";
+
+const HomePage = () => {
+  return (
+    <div className="">
+      <header>
+        Header
+        <div>
+          <Button>Click me</Button>
+        </div>
+      </header>
+      Home page
+    </div>
+  );
+};
+
+export default HomePage;
