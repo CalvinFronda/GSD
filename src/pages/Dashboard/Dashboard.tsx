c85d26d192e50998c0f397ce24ed864ac6fdc7ae
Calvin Fronda <cfronda209@gmail.com>
2025-05-29 14:03:21 -0700

    ✨ New Dashboard
    Implement new dashboard design with dummy data

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,23 +1,61 @@
-import { TaskDialog } from "@/pages/Dashboard/children/addtaskdialog";
-import TaskCard from "@/pages/Dashboard/children/taskcard";
-
 import { useTaskStore } from "@/store/useTaskStore";
 import { useFetchTasks } from "@/hooks/useFetchTasks";
+import { TASK_STATUS_TYPE } from "@/constants/firestore.constants";
+import TopDashboardWidget from "./children/TopDashboardWidget";
+import { Clipboard, InboxIcon } from "lucide-react";
+
+import { WeeklyTaskChart, TaskDistro } from "./children/DashboardCharts";
+// import QuickActions from "./children/QuickActions";
+// import TodaysFocus from "./children/TodaysFocus";
 
 export default function Dashboard() {
   const tasks = useTaskStore((s) => s.tasks);
 
   useFetchTasks();
 
+  const inbox = tasks.filter(
+    (task) => task.status === TASK_STATUS_TYPE.NOT_STARTED,
+  );
+
   return (
-    <div className="flex flex-col gap-4">
-      <div className="w-1/4">
-        <TaskDialog />
+    <div className="w-full mx-auto">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-8">
+        <TopDashboardWidget
+          title="Inbox"
+          count={inbox.length}
+          Icon={<InboxIcon className="text-blue-500" />}
+          meta={{ color: "red", text: "unprocessed", count: 0 }}
+        />
+        <TopDashboardWidget
+          title="Next Actions"
+          count={inbox.length}
+          Icon={<Clipboard className="text-green-500" />}
+          meta={{ color: "green", text: "Due today", count: 3 }}
+        />
+        <TopDashboardWidget
+          title="Projects"
+          count={inbox.length}
+          Icon={<Clipboard className="text-purple-500" />}
+          meta={{ color: "purple", text: "Active", count: 3 }}
+        />
+        <TopDashboardWidget
+          title="Waiting For"
+          count={inbox.length}
+          Icon={<Clipboard className="text-yellow-500" />}
+          meta={{ color: "yellow", text: "Active", count: 3 }}
+        />
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-5 gap-4">
-        {tasks.map((task, i) => (
-          <TaskCard key={i} task={task} />
-        ))}
+
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 mb-8">
+        <WeeklyTaskChart />
+
+        <TaskDistro />
+      </div>
+
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+        {/* Might not use this <QuickActions /> */}
+
+        {/* Might not use this <TodaysFocus /> */}
       </div>
     </div>
   );
